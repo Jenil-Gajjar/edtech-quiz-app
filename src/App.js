86247191100a400fca87472bc9ff1b8d43d991b1
JenliGajjar23 <tatva.pca38@outlook.com>
2025-07-14 18:43:10 +0530

    Added sign in and sign up component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,145 @@
-import { version, Component } from 'inferno';
-import Logo from './logo.js';
-import './App.css';
+// import "./App.css";
+import { BrowserRouter, Route, Switch } from "inferno-router";
+import {
+  Auth,
+  SignInComponent,
+  SignUpComponent,
+} from "./components/auth/Auth.jsx";
 
-export default class App extends Component {
-  render() {
-    return (
+// import { Component, linkEvent } from "inferno";
+export default function App() {
+  return (
+    <BrowserRouter>
       <div className="App">
-        <header className="App-header">
-          <Logo width="80" height="80" />
-          <p>{`Welcome to Inferno ${version}`}</p>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
+        <Switch>
+          {/* <Route path={["/", "/login"]} exact>
+            <AuthLayout>
+            </AuthLayout>
+          </Route>
+
+          <Route path="/register" exact>
+            <AuthLayout>
+            </AuthLayout>
+          </Route>
+
+          <Route path="/user-form">
+            <MainLayout>
+            </MainLayout>
+          </Route>
+
+          <Route path="/question1">
+            <MainLayout Component={Question1} />
+          </Route>
+
+          <Route path="/question2">
+            <MainLayout Component={Question2} />
+          </Route>
+
+          <Route path="/user-profile">
+            <MainLayout>
+              <MainLayout Component={UserProfile} />
+            </MainLayout>
+          </Route> */}
+
+          {/* <Route path="/user-form" component={UserForm} /> */}
+          {/* <Route path="/question1" component={Question1} /> */}
+          {/* <Route path="/question2" component={Question2} /> */}
+          {/* <Route
+            path="/user-profile"
+            component={() => <UserProfile name="Jenil" age={21} />}
+          /> */}
+          <Route path={["/", "/sign-in"]} exact>
+            <Auth>
+              <SignInComponent />
+            </Auth>
+          </Route>
+
+          <Route path="/sign-up">
+            <Auth>
+              <SignUpComponent />
+            </Auth>
+          </Route>
+          {/* <Route component={NotFound} /> */}
+        </Switch>
       </div>
-    );
-  }
+    </BrowserRouter>
+  );
 }
+
+// const NotFound = () => {
+//   return (
+//     <div style={{ margin: "0 20px", color: "blue" }}>
+//       <h1>404</h1>
+//       <div>Not Found</div>
+//     </div>
+//   );
+// };
+
+// const MainLayout = ({ Component, ...props }) => {
+//   return (
+//     <div className="main-container">
+//       <NavBar />
+//       <Component {...props} />
+//     </div>
+//   );
+// };
+
+// const AuthLayout = (props) => {
+//   return <div className="auth-container">{props.children}</div>;
+// };
+
+// class UserProfile extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       name: props.name || "",
+//     };
+//     // alternative of link event
+//     // this.handleInput = this.handleInput.bind(this);
+//   }
+
+//   handleInput(component, event) {
+//     component.setState({ name: event.target.value });
+//   }
+
+//   render() {
+//     return (
+//       <div style={{ margin: "0 20px" }}>
+//         <label htmlFor="name">Name: </label>
+
+//         <input
+//           id="name"
+//           value={this.state.name}
+//           onInput={linkEvent(this, this.handleInput)}
+//           placeholder="Enter your name"
+//         />
+//         <div>Name: {this.state.name}</div>
+//         <div>Age: {this.props.age}</div>
+//       </div>
+//     );
+//   }
+// }
+
+// const NavBar = () => {
+//   return (
+//     <nav>
+//       <ul>
+//         <li>
+//           <Link to="/">Home</Link>
+//         </li>
+//         <li>
+//           <Link to="user-form">User Form</Link>
+//         </li>
+//         <li>
+//           <Link to="question1">Question 1</Link>
+//         </li>
+//         <li>
+//           <Link to="question2">Question 2</Link>
+//         </li>
+//         <li>
+//           <Link to="user-profile">Profile Page</Link>
+//         </li>
+//       </ul>
+//     </nav>
+//   );
+// };
---
+++ src/components/auth/Auth.jsx
@@ -0,0 +1,247 @@
+import { Link } from 'inferno-router'
+import '../../assets/css/Auth.css'
+
+const apiUrl = "http://localhost:5051/api/Auth";
+
+const UsernameRegex = /^[a-zA-Z]{3}[a-zA-Z0-9_-]{5,17}$/;
+const UsernameErrorMessage = "Username must be 8-20 characters long and can only contain letters, numbers, underscores, or hyphens.";
+
+const PasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+const PasswordErrorMessage = "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character.";
+
+const EmailRegex = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.[a-zA-Z]{2,}([-.]\w+)*$/
+const EmailErrorMessage = "Invalid Email Address."
+
+export function Auth({ children }) {
+    return (
+        <div className="Auth-Container">
+            <div className="card">
+                {children}
+            </div>
+        </div>
+    )
+}
+
+function ErrorHandler(id, msg) {
+    document.getElementById(id).innerText = msg;
+}
+
+function ValidationHandler(id, msg, regex, event) {
+    if (!regex.test(event.target.value)) {
+        ErrorHandler(id, msg)
+    } else {
+        ErrorHandler(id, null)
+    }
+}
+
+export function SignInComponent() {
+
+    const handleSubmission = (e) => {
+        e.preventDefault()
+        var formData = new FormData(e.target)
+        var username = formData.get('Username');
+        var password = formData.get('Password');
+        var data = { Username: username, Password: password };
+
+        if (!username) {
+            ErrorHandler('UsernameErrorMessage', 'Username is required');
+            return;
+        }
+        if (!password) {
+            ErrorHandler('PasswordErrorMessage', 'Password is required');
+            return;
+        }
+
+        if (!UsernameRegex.test(username)) {
+            ErrorHandler('UsernameErrorMessage', UsernameErrorMessage);
+            return;
+        }
+        if (!PasswordRegex.test(password)) {
+            ErrorHandler('PasswordErrorMessage', PasswordErrorMessage);
+            return;
+        }
+
+        const url = apiUrl + '/SignIn'
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(response => {
+                const data = response.json();
+                console.log(data)
+                return ({
+                    status: response.status,
+                    data: data
+                })
+            })
+            .then(({ status, data }) => {
+                if (status === 200) {
+                    window.location.replace('/dashboard')
+                } else {
+                    ErrorHandler('PasswordErrorMessage', data.message || 'Invalid username or password');
+                }
+            })
+            .catch(error => {
+                console.error(error)
+                ErrorHandler('PasswordErrorMessage', 'Something went wrong!');
+            })
+    }
+    return (
+        <div className="px-4 py-5 text-center" >
+            <span className='fs-3 fw-semibold'>EdTech Quiz App</span>
+            <div className="px-sm-5 pb-3">
+                <form className='container' onSubmit={handleSubmission}>
+                    <p className='fs-4'>Signin</p>
+                    <div className='Input-Group-Parent'>
+                        <div className="input-group" >
+                            <span className="input-group-text Auth-InputGroup-Text px-3" ><i className="bi bi-person"></i></span>
+                            <input type="text" className="form-control Auth-Input" name='Username' placeholder="Username" aria-label="Username" required
+                                onInput={(event) => { ValidationHandler('UsernameErrorMessage', UsernameErrorMessage, UsernameRegex, event) }}
+                            />
+                        </div>
+                        <span className='text-danger text-start' id='UsernameErrorMessage' ></span>
+                    </div>
+
+                    <div className='Input-Group-Parent'>
+                        <div className="input-group" >
+                            <span className="input-group-text Auth-InputGroup-Text px-3"><i className="bi bi-lock "></i></span>
+                            <input type="password" className="form-control Auth-Input" name='Password' placeholder="Password" aria-label="password" autoComplete='off' required
+                                onInput={(event) => { ValidationHandler('PasswordErrorMessage', PasswordErrorMessage, PasswordRegex, event) }}
+                            />
+                        </div>
+                        <span className='text-danger text-start' id='PasswordErrorMessage' ></span>
+                    </div>
+                    <input type="submit" value="Signin" className='Auth-btn' />
+                </form>
+            </div>
+            <div className='fs-5'>
+                <span>
+                    Don&apos;t have an account?
+                    <Link to="/sign-up" className='fw-bold text-dark text-decoration-none Pointer ms-1'>Signup</Link>
+                </span>
+            </div>
+        </div>
+    )
+}
+
+export function SignUpComponent() {
+
+    const handleSubmission = (e) => {
+        e.preventDefault()
+        var formData = new FormData(e.target)
+        var username = formData.get('Username')
+        var email = formData.get('Email')
+        var password = formData.get('Password')
+
+        var data = {
+            Username: username,
+            Email: email,
+            Password: password
+        }
+
+        if (!username) {
+            ErrorHandler('UsernameErrorMessage', 'Username is required');
+            return;
+        }
+        if (!password) {
+            ErrorHandler('PasswordErrorMessage', 'Password is required');
+            return;
+        }
+        if (!email) {
+            ErrorHandler('EmailErrorMessage', EmailErrorMessage);
+            return;
+        }
+        if (!UsernameRegex.test(username)) {
+            ErrorHandler('UsernameErrorMessage', UsernameErrorMessage);
+            return;
+        }
+        if (!PasswordRegex.test(password)) {
+            ErrorHandler('PasswordErrorMessage', PasswordErrorMessage);
+            return;
+        }
+        if (!EmailRegex.test(email)) {
+            ErrorHandler('EmailErrorMessage', EmailErrorMessage);
+            return;
+        }
+
+        const url = apiUrl + '/SignUp'
+
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+            .then(response => {
+                const data = response.json();
+                return ({
+                    status: response.status,
+                    data: data,
+                });
+            })
+            .then(({ status, data }) => {
+                if (status === 201 && data.isSuccess) {
+                    window.location.replace('/sign-in')
+                }
+                else {
+                    ErrorHandler('PasswordErrorMessage', data.message || 'Username or email already exists');
+                }
+            })
+            .catch(error => {
+                console.error(error)
+                ErrorHandler('PasswordErrorMessage', 'Something went wrong!');
+            })
+    }
+    return (
+        <div className="p-5 text-center" >
+            <span className='fs-3 fw-semibold'>EdTech Quiz App</span>
+            <div className="px-sm-5 pb-3">
+                <form onSubmit={handleSubmission}>
+                    <p className='fs-4'>Signup</p>
+
+                    <div className='Input-Group-Parent'>
+                        <div className="input-group" >
+                            <span className="input-group-text Auth-InputGroup-Text px-3" ><i className="bi bi-person "></i></span>
+                            <input type="text" className="form-control Auth-Input" name="Username" placeholder="Username" aria-label="Username" required
+                                onInput={(event) => { ValidationHandler('UsernameErrorMessage', UsernameErrorMessage, UsernameRegex, event) }}
+                            />
+                        </div>
+                        <span className='text-danger' id='UsernameErrorMessage'></span>
+                    </div>
+
+                    <div className='Input-Group-Parent'>
+                        <div className="input-group" >
+                            <span className="input-group-text Auth-InputGroup-Text px-3" ><i className="bi bi-envelope "></i></span>
+                            <input type="email" className="form-control Auth-Input" name="Email" placeholder="Email" aria-label="Email" required
+                                onInput={(event) => { ValidationHandler('EmailErrorMessage', EmailErrorMessage, EmailRegex, event) }}
+                            />
+                        </div>
+                        <span className='text-danger' id='EmailErrorMessage'></span>
+                    </div>
+
+                    <div className='Input-Group-Parent'>
+                        <div className="input-group ">
+                            <span className="input-group-text Auth-InputGroup-Text px-3 "><i className="bi bi-lock "></i></span>
+                            <input type="password" className="form-control Auth-Input" name='Password' placeholder="Password" aria-label="Password" autoComplete='off' required
+                                onInput={(event) => { ValidationHandler('PasswordErrorMessage', PasswordErrorMessage, PasswordRegex, event) }}
+                            />
+                        </div>
+                        <span className='text-danger' id='PasswordErrorMessage' style="width:100px"></span>
+                    </div>
+
+                    <input type="submit" value="Signup" className='Auth-btn' />
+                </form>
+            </div>
+            <div className='fs-5'>
+                <span>
+                    Already have an account?
+                    <Link to="/sign-in" className='fw-bold text-dark text-decoration-none Pointer ms-1'>Signin</Link>
+                </span>
+            </div>
+        </div>
+    )
+}
